6eaf5605d7b29369ba77b13e5f55a95b61ec1513
joaojpsa <joaojpsa@gmail.com>
2022-03-05 16:01:01 -0300

    variaveis em js

---
+++ Introdução ao Javascript/particularidades-js/js/scripts.js
@@ -0,0 +1,17 @@
+console.log("primeiro");
+
+var teste = 1;
+console.log(teste);
+teste = "qualquer nome";
+console.log(teste)
+
+let testandoLet = "ok";
+const testandoConst = "blz";
+console.log(testandoLet, testandoConst);
+
+var nome;
+console.log(nome);
+nome = "joao";
+console.log(nome);
+
+console.log("ultimo");
---
+++ Introdução ao Javascript/tipos_de_dados/js/scripts.js
@@ -0,0 +1,9 @@
+//NUMBER
+var numero = 5;
+console.log(typeof numero, numero);
+var numeroFloat = 5.5;
+console.log(typeof numeroFloat, numeroFloat);
+
+var textoNumero = "54";
+var junto = numero + textoNumero;
+console.log(typeof junto, junto);
